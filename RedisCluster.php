e297c2e99d8699e0ea98857e2d4550caa62024f6
Julien <ysbaddaden@gmail.com>
2010-03-03 16:36:14 +0100

    Started work on a RedisCluster class (not working yet).

---
+++ RedisCluster.php
@@ -0,0 +1,65 @@
+<?php
+
+# Shards keys within multiple Redis servers.
+# 
+# When using a cluster of servers, some redis commands may lead
+# to problems (eg: multi/exec, mset, mget, etc), since keys must be on the
+# same servers for these to work.
+# 
+# IMPROVE: implement mset/mget methods so that keys could still be sharded
+# among servers.
+# 
+# =Key Hashing
+# 
+# For commands like SORT to work, all matching keys are expected to be in a
+# single Redis server. For assigning the same type of keys to the same server
+# you must specify your own hashing method.
+# 
+# For instance the following method will only encode the begining of the key
+# (whatever is before +:+), so that all +webcomic:*+ keys will be on the same
+# servers, while +chapters:*+ could be on another one:
+# 
+#   $redis = new RedisCluster($configs, function($key)
+#   {
+#     $hkey = explode(':', $key, 2);
+#     return md5($hkey[0]);
+#   });
+# 
+class RedisCluster
+{
+  private $redis;
+  private $hash_method;
+  
+  function __construct($configs, $hash_method='md5')
+  {
+    if (count($configs) == 0) {
+      trigger_error("You must configure at least one redis server.", E_USER_ERROR);
+    }
+    $this->servers     = new RedisServers($configs);
+    $this->hash_method = $hash_method;
+  }
+  
+  function __call($func, $args)
+  {
+    $server = $this->hash($func, $args);
+    return $this->send_command($server, $func, $args);
+  }
+  
+  # Sends a command to a specific server.
+  function send_command($server, $command, $args) {
+    return $this->servers[$server]->$func($args);
+  }
+  
+  private function hash($func, $args)
+  {
+    if (count($this->servers) > 1)
+    {
+      $cmd  = Redis::lookup_command($func, $args);
+      $hash = call_user_func_array($this->hash_method, $args[$cmd['key']]);
+      return $hash % count($this->servers);
+    }
+    return 0;
+  }
+}
+
+?>
---
+++ RedisServers.php
@@ -0,0 +1,50 @@
+<?php
+
+# Handles the list of servers for <tt>RedisCluster</tt>.
+class RedisServers implements Countable, ArrayAccess
+{
+  private $servers = array();
+  private $configs;
+  private $count;
+  
+  function __construct($configs)
+  {
+    $this->configs = $configs;
+    $this->count   = count($configs);
+  }
+  
+  function count() {
+    return $this->count;
+  }
+  
+  function offsetExists($index) {
+    return isset($this->configs[$index]);
+  }
+  
+  function offsetSet($index, $value) {
+    trigger_error("Operation is disabled.", E_USER_ERROR);
+  }
+  
+  function offsetGet($index)
+  {
+    if (!isset($this->servers[$index]))
+    {
+      if (!isset($this->configs[$index]))
+      {
+        trigger_error("No configuration for server $index, using localhost:6379.", E_USER_WARNING);
+        $config = array('host' => 'localhost', 'port' => 6379);
+      }
+      else {
+        $config = $this->configs[$index];
+      }
+      $this->servers[$index] = new Redis($config);
+    }
+    return $this->servers[$index];
+  }
+  
+  function offsetUnset($index) {
+    unset($this->servers[$index]);
+  }
+}
+
+?>

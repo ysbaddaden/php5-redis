86111a3db83e21c9d1b881be661fb5dc14246294
Julien <ysbaddaden@gmail.com>
2010-03-26 17:21:32 +0100

    Refactored RedisCluster->pipeline() which is now exploded in a few private methods.

--- RedisCluster.php
+++ RedisCluster.php
@@ -59,15 +59,81 @@ class RedisCluster
     return $result;
   }
   
+  # FIXME: MGET in PIPELINE won't return replies in the right order.
   function pipeline($closure)
   {
-    # executes the closure
     $pipe = new RedisPipeline($this);
     $closure($pipe);
     
-    # dispatches commands by server (keeping the index)
+    # executes the commands and dispatches results by their original index
+    $commands_by_server = $this->dispatch_commands_by_server($pipe->commands());
+    $rs = array();
+    foreach($commands_by_server as $server => $commands)
+    {
+      $replies = $this->send_commands($server, $commands);
+      
+      if (!is_array($replies)) {
+        $this->set_dispatched_reply($rs, key($commands), $replies);
+      }
+      else
+      {
+        foreach($replies as $reply)
+        {
+          $this->set_dispatched_reply($rs, key($commands), $reply);
+          next($commands);
+        }
+      }
+    }
+    ksort($rs);
+    return $rs;
+  }
+  
+  function mset($keys) {
+    return $this->_mset($keys);
+  }
+  
+  function msetnx($keys) {
+    return $this->_mset($keys, true);
+  }
+  
+  private function _mset($keys, $nx=false)
+  {
+    $cmd = "mset".($nx ? 'nx' : '');
+    $keys_by_server = array();
+    foreach($keys as $key => $value)
+    {
+      $server = $this->hash($cmd, $key);
+      $keys_by_server[$server][$key] = $value;
+    }
+    
+    $rs = true;
+    foreach($keys_by_server as $server => $args) {
+      $rs = $rs && !!$this->send_command($server, $cmd, array($args));
+    }
+    return $rs;
+  }
+  
+  private function set_dispatched_reply(&$rs, $i, $reply)
+  {
+    if (!isset($rs[$i])) {
+      $rs[$i] = $reply;
+    }
+    elseif (is_bool($reply)) {
+      $rs[$i] = $rs[$i] and $reply;
+    }
+    elseif (is_array($reply)) {
+      $rs[$i] = array_merge($rs[$i], $reply);
+    }
+    else {
+      trigger_error("Unknown reply type '".gettype($reply)."'", E_USER_WARNING);
+    }
+  }
+  
+  # Dispatches commands by server (keeping the index for merging the replies correctly).
+  private function & dispatch_commands_by_server(&$commands)
+  {
     $commands_by_server = array();
-    foreach($pipe->commands() as $i => $cmd)
+    foreach($commands as $i => $cmd)
     {
       switch($cmd[0])
       {
@@ -103,67 +169,7 @@ class RedisCluster
           $commands_by_server[$server][$i] = $cmd;
       }
     }
-    
-    # executes the commands and dispatches results by their original index
-    $rs = array();
-    foreach($commands_by_server as $server => $commands)
-    {
-      $replies = $this->send_commands($server, $commands);
-      
-      if (!is_array($replies)) {
-        $rs[key($commands)] = $replies;
-      }
-      else
-      {
-        foreach($replies as $reply)
-        {
-          $i = key($commands);
-          if (!isset($rs[$i])) {
-            $rs[$i] = $reply;
-          }
-          else
-          {
-            if (is_bool($reply)) {
-              $rs[$i] = $rs[$i] and $reply;
-            }
-            elseif (is_array($reply)) {
-              $rs[$i] = array_merge($rs[$i], $reply);
-            }
-            else {
-              trigger_error("Unknown reply type '".gettype($rs)."'", E_USER_WARNING);
-            }
-          }
-          next($commands);
-        }
-      }
-    }
-    ksort($rs);
-    return $rs;
-  }
-  
-  function mset($keys) {
-    return $this->_mset($keys);
-  }
-  
-  function msetnx($keys) {
-    return $this->_mset($keys, true);
-  }
-  
-  private function _mset($keys, $nx=false)
-  {
-    $cmd = "mset".($nx ? 'nx' : '');
-    $keys_by_server = array();
-    foreach($keys as $key => $value)
-    {
-      $server = $this->hash($cmd, $key);
-      $keys_by_server[$server][$key] = $value;
-    }
-    
-    $rs = true;
-    foreach($keys_by_server as $server => $args) {
-      $rs = $rs && !!$this->send_command($server, $cmd, array($args));
-    }
-    return $rs;
+    return $commands_by_server;
   }
   
   # Sends a command to a specific server.
